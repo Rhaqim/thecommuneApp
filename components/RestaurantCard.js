ff46b98ecf06160d6cc624a29d5bdd838df79cbe
Rhaqim <anusiemj@gmail.com>
2022-08-09 22:41:56 +0100

    working on the features section

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const RestaurantCard = ({ id, imgUrl, title, rating, genre, address, short_description, dishes, long, lat }) => {
+const RestaurantCard = ({ id, 
+    imgUrl, 
+    title, 
+    rating, 
+    genre, 
+    address, 
+    short_description, 
+    dishes, 
+    long, 
+    lat }) => {
     return (
-        <View>
-            <Text>RestaurantCard</Text>
-        </View>
+        <TouchableOpacity>
+            <Image 
+            source={{
+                uri: imgUrl
+            }}
+            className="w-64 h-36 rounded-sm"
+            />
+            <View>
+                <Text className="text-sm font-bold">{title}</Text>
+            </View>
+        </TouchableOpacity>
     )
 }
 
