e47f87eb0298b5083ab89df6310f6a46fa1e9019
Rhaqim <anusiemj@gmail.com>
2022-08-11 04:32:55 +0100

    completed restaurants cards with ratings and location, working with sanity.io for data

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -31,6 +31,45 @@ const FeaturedRow = ({ id, title, description }) => {
                     long={20}
                     lat={0}
                 />
+                <RestaurantCard
+                    id={12}
+                    imgUrl={"https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
+                    // imgUrl={"https://links.papareact.com/gn7"}
+                    title="Shiro Lagos"
+                    rating={4.5}
+                    genre="Japanese"
+                    address="123 Main street"
+                    short_description="Japanese themed restaurant in Lagos"
+                    dishes={[]}
+                    long={20}
+                    lat={0}
+                />
+                <RestaurantCard
+                    id={12}
+                    imgUrl={"https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
+                    // imgUrl={"https://links.papareact.com/gn7"}
+                    title="Shiro Lagos"
+                    rating={4.5}
+                    genre="Japanese"
+                    address="123 Main street"
+                    short_description="Japanese themed restaurant in Lagos"
+                    dishes={[]}
+                    long={20}
+                    lat={0}
+                />
+                <RestaurantCard
+                    id={12}
+                    imgUrl={"https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
+                    // imgUrl={"https://links.papareact.com/gn7"}
+                    title="Shiro Lagos"
+                    rating={4.5}
+                    genre="Japanese"
+                    address="123 Main street"
+                    short_description="Japanese themed restaurant in Lagos"
+                    dishes={[]}
+                    long={20}
+                    lat={0}
+                />
             </ScrollView>
         </View>
     )
